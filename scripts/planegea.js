a43fb86467dbce8707bc3c9f8a5ca8ccc08daacf
John Weland <jweland@vtt.weland.homelab>
2024-11-20 21:06:38 +0000

    init commit

---
+++ scripts/planegea.js
@@ -0,0 +1,11 @@
+Hooks.once("init", () => {
+    CONFIG.DND5E.tools.butcher =
+        "Compendium.planegea.items.Item.9KM8gNt3FsNh7iI4";
+    CONFIG.DND5E.currencies.ps = {
+        label: "Salt",
+        abbreviation: "ps",
+        conversion: 1,
+        icon: "systems/dnd5e/icons/currency/silver.webp",
+    };
+    CONFIG.DND5E.languages.standard.children.saurian = "Saurian";
+});
