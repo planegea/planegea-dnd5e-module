46c8e470ae2fad75817bbad577235c376943310e
John Weland <jweland@vtt.weland.homelab>
2024-11-21 03:46:05 +0000

    added tools, added more backgrounds

--- scripts/planegea.js
+++ scripts/planegea.js
@@ -1,6 +1,8 @@
 Hooks.once("init", () => {
     CONFIG.DND5E.tools.butcher =
         "Compendium.planegea.items.Item.9KM8gNt3FsNh7iI4";
+    CONFIG.DND5E.tools.gather =
+        "Compendium.planegea.items.Item.8P3VsvqHWJmRnanY";
     CONFIG.DND5E.currencies.ps = {
         label: "Salt",
         abbreviation: "ps",
