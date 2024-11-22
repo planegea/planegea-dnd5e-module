2bb9cf9a58d74060270463207e7df9d6a8316e6b
John Weland <jweland@vtt.weland.homelab>
2024-11-22 20:07:28 +0000

    added all SSSoP spells, added page attributions to them

--- scripts/planegea.js
+++ scripts/planegea.js
@@ -3,11 +3,13 @@ Hooks.once("init", () => {
         "Compendium.planegea.items.Item.9KM8gNt3FsNh7iI4";
     CONFIG.DND5E.tools.gather =
         "Compendium.planegea.items.Item.8P3VsvqHWJmRnanY";
-    CONFIG.DND5E.currencies.ps = {
-        label: "Salt",
-        abbreviation: "ps",
-        conversion: 1,
-        icon: "systems/dnd5e/icons/currency/silver.webp",
+    CONFIG.DND5E.currencies = {
+        ps: {
+            label: "Salt",
+            abbreviation: "ps",
+            conversion: 1,
+            icon: "modules/planegea/media/icons/currency/salt.webp",
+        },
     };
     CONFIG.DND5E.languages.standard.children.saurian = "Saurian";
 });
